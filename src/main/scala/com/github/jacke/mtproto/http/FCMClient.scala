b046ea86e3982b1fbca0934d21cfaf2b76abd2d3
Stan <iamjacke@gmail.com>
2018-12-02 22:21:53 +0300

    Add payload example

--- src/main/scala/com/github/jacke/mtproto/http/FCMClient.scala
+++ src/main/scala/com/github/jacke/mtproto/http/FCMClient.scala
@@ -5,6 +5,18 @@ import com.softwaremill.sttp.akkahttp._
 import scala.concurrent.Future
 import scala.util.{ Failure, Success }
 
+/* Example payload:
+
+{
+    "notification": {
+        "title": "Firebase",
+        "body": "Firebase is awesome",
+        "click_action": "http://localhost:3000/",
+        "icon": "http://url-to-an-icon/icon.png"
+    },
+    "to": "chUEblRLVl4:APA91bFF3XG4836K3fIe3INGInEMqwbh2JGCYj9TE2iLfkUCvOApVncNJajagAh_YcxwzCAO3aaUOzXUvGKaLauGvzoH3_W7s3blN7aJwN7B72qSOx-8OIznwfQu0esWOeBnk4UJn9El"
+}
+*/
 object FCMClient {
   def push(payload: String): Future[Response[String]] = {
     implicit val backend = AkkaHttpBackend()
